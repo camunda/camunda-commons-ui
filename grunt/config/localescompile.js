7100154f7bc79b5de2f7238511d3723879bb2e4c
Valentin Vago <zeropaper@irata.ch>
2014-07-30 10:33:46 +0200

    feat(locales): add a task and default config for locales compilation

---
+++ grunt/config/localescompile.js
@@ -0,0 +1,19 @@
+module.exports = function() {
+  'use strict';
+  return {
+    options: {
+      dest: '<%= buildTarget %>/locales'
+    },
+    prod: {
+      options: {
+        onlyProd: 1,
+        anOption: 'for production'
+      },
+      src: [
+        'node_modules/camunda-commons-ui/resources/locales/*.json',
+        'node_modules/camunda-commons-ui/lib/*/locales/*.json',
+        'client/scripts/*/locales/*.json'
+      ]
+    }
+  };
+};
---
+++ grunt/tasks/localescompile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+  'use strict';
+  var path = require('path');
+
+  grunt.registerMultiTask('localescompile', function() {
+    var lang, file, f, loaded, key, filepath;
+    var done = this.async();
+    var languages = {};
+    var dest = grunt.config.get(this.name +'.'+ this.target +'.options.dest');
+    dest = dest || grunt.config.get(this.name +'.options.dest');
+
+    for (f in this.filesSrc) {
+      file = this.filesSrc[f];
+      lang = path.basename(file, '.json');
+
+      languages[lang] = languages[lang] || {};
+
+      loaded = require(file);
+      for (key in loaded) {
+        languages[lang][key] = loaded[key];
+      }
+    }
+
+    for (lang in languages) {
+      filepath = path.join(dest, lang +'.json');
+      grunt.file.write(filepath, JSON.stringify(languages[lang], null, 2));
+      grunt.verbose.writeln('Wrote "%s" translations in %s', lang, filepath);
+    }
+
+    done();
+  });
+};
