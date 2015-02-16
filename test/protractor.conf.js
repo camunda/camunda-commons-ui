749bb43bb588e675cce1acc1f108911161a6d089
Valentin Vago <zeropaper@irata.ch>
2015-02-16 08:01:46 +0100

    feat(loader widget); add the loader widget
    Related to CAM-3434

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -8,7 +8,8 @@ exports.config = {
   specs: [
     '../lib/widgets/inline-field/test/cam-widget-inline-field.spec.js',
     '../lib/widgets/search-pill/test/search-pill.spec.js',
-    '../lib/widgets/header/test/cam-widget-header.spec.js'
+    '../lib/widgets/header/test/cam-widget-header.spec.js',
+    '../lib/widgets/loader/test/cam-widget-loader.spec.js'
   ],
 
   framework: 'jasmine2',
