cd95ea0e90e712beaa050a9c52bf392df4f823ca
Sebastian Stamm <sebastian.stamm@camunda.com>
2015-03-02 13:35:49 +0100

    feat(widgets): add bpmn viewer widget
    related to CAM-3550

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -10,7 +10,8 @@ exports.config = {
     '../lib/widgets/search-pill/test/search-pill.spec.js',
     '../lib/widgets/header/test/cam-widget-header.spec.js',
     '../lib/widgets/loader/test/cam-widget-loader.spec.js',
-    '../lib/widgets/search/test/cam-widget-search.spec.js'
+    '../lib/widgets/search/test/cam-widget-search.spec.js',
+    '../lib/widgets/bpmn-viewer/test/cam-widget-bpmn-viewer.spec.js'
   ],
 
   framework: 'jasmine2',
