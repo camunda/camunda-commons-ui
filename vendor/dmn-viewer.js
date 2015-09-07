edbefe5d28d27d0f552e7c78a433b5f64bc44f58
Sebastian Stamm <sebastian.stamm@camunda.com>
2015-09-07 09:10:35 +0200

    WIP: dmn-viewer-widget

--- vendor/dmn-viewer.js
+++ vendor/dmn-viewer.js
@@ -8,7 +8,7 @@
  *
  * Source Code: https://github.com/dmn-io/dmn-js
  *
- * Date: 2015-09-04
+ * Date: 2015-09-07
  */
 
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.DmnJS=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
@@ -927,9 +927,18 @@ function MappingsRow(eventBus, sheet, elementRegistry, graphicsFactory, complexC
   var positionTemplate = function(node, element, large) {
     var table = sheet.getRootElement();
     if(large) {
+      var e = table;
+      var offset = {x:0,y:0};
+      while (e)
+      {
+          offset.x += e.offsetLeft;
+          offset.y += e.offsetTop;
+          e = e.offsetParent;
+      }
+
       assign(node.style, {
-        top: table.offsetTop + 'px',
-        left: table.offsetLeft + 'px',
+        top: offset.y + 'px',
+        left: offset.x + 'px',
         width: table.clientWidth + 'px'
       });
     } else {
