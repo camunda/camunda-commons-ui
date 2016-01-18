dcf01725871709700e3d86ebdade037447f153e4
Valentin Vago <zeropaper@irata.ch>
2016-01-18 10:49:11 +0100

    refactor(styles): rework imports
    chore(styles): update grunt less task
    refactor(styles): add inlcude paths for dmn-js in less task
    refactor(styles): add commonly used styles to cam-common.less
    refactor(styles): move dmn-viewer styles to widget styles
    refactor(styles): work on commonly shared styles
    refactor(styles): make header compatible with tasklist plugins
    improve(styles): set size to glyphicons within h1
    
    Related to CAM-5265

--- grunt/config/less.js
+++ grunt/config/less.js
@@ -1,17 +1,26 @@
 module.exports = function(config, lessConfig, pathConfig) {
   'use strict';
+  var resolve = require('path').resolve;
 
   var file = {};
-  file[pathConfig.buildTarget+'/styles/styles.css'] = pathConfig.sourceDir+'/styles/styles.less';
+  file[pathConfig.buildTarget+'/styles/styles.css'] = pathConfig.sourceDir + '/styles/styles.less';
+
+  var includePaths = [
+    resolve(process.cwd(), 'node_modules/camunda-commons-ui/node_modules/bootstrap/less'),
+    resolve(process.cwd(), 'node_modules/camunda-commons-ui/lib/widgets'),
+    resolve(process.cwd(), 'node_modules/camunda-commons-ui/resources/less'),
+    resolve(process.cwd(), 'node_modules/camunda-commons-ui/resources/css'),
+    resolve(process.cwd(), 'node_modules/camunda-commons-ui/node_modules'),
+    resolve(process.cwd(), 'node_modules', 'camunda-' + pathConfig.appName +'-ui', 'styles'),
+    resolve(process.cwd(), 'node_modules', 'camunda-' + pathConfig.appName +'-ui', 'client/scripts'),
+
+    resolve(__dirname, '../../..', 'camunda-bpm-webapp/webapp/src/main/resources-plugin'),
+    resolve(__dirname, '../../..', 'camunda-bpm-platform-ee/webapps/camunda-webapp/plugins/src/main/resources-plugin')
+  ];
 
   lessConfig[pathConfig.appName + '_styles'] = {
     options: {
-      paths: [
-        'node_modules',
-        'node_modules/camunda-commons-ui/node_modules',
-        'styles',
-        'scripts'
-      ],
+      paths: includePaths,
 
 
       dumpLineNumbers: '<%= buildMode === "prod" ? "" : "comments" %>',
@@ -19,7 +28,7 @@ module.exports = function(config, lessConfig, pathConfig) {
       sourceMap: '<%= buildMode === "prod" ? "true" : "" %>',
 
       sourceMapURL: './styles.css.map',
-      sourceMapFilename: pathConfig.buildTarget+'/styles/styles.css.map'
+      sourceMapFilename: pathConfig.buildTarget + '/styles/styles.css.map'
     },
     files: file
   };
