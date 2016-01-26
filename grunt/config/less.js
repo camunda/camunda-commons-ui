be9ffd2b55882e8f2a4fe9426c98bd0b3457536b
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-01-26 14:56:52 +0100

    chore(less): use path to commons-ui when compiling less

--- grunt/config/less.js
+++ grunt/config/less.js
@@ -6,23 +6,18 @@ module.exports = function(config, lessConfig, pathConfig) {
   file[pathConfig.buildTarget+'/styles/styles.css'] = pathConfig.sourceDir + '/styles/styles.less';
 
   var includePaths = [
-    resolve(process.cwd(), 'node_modules/camunda-commons-ui/node_modules/bootstrap/less'),
-    resolve(process.cwd(), 'node_modules/camunda-commons-ui/lib/widgets'),
-    resolve(process.cwd(), 'node_modules/camunda-commons-ui/resources/less'),
-    resolve(process.cwd(), 'node_modules/camunda-commons-ui/resources/css'),
-    resolve(process.cwd(), 'node_modules/camunda-commons-ui/node_modules'),
-    resolve(process.cwd(), 'node_modules/camunda-bpm-webapp/node_modules/camunda-commons-ui/node_modules/bootstrap/less'),
-    resolve(process.cwd(), 'node_modules/camunda-bpm-webapp/node_modules/camunda-commons-ui/lib/widgets'),
-    resolve(process.cwd(), 'node_modules/camunda-bpm-webapp/node_modules/camunda-commons-ui/resources/less'),
-    resolve(process.cwd(), 'node_modules/camunda-bpm-webapp/node_modules/camunda-commons-ui/resources/css'),
-    resolve(process.cwd(), 'node_modules/camunda-bpm-webapp/node_modules/camunda-commons-ui/node_modules'),
+    resolve(process.cwd(), '<%= pkg.gruntConfig.commonsUiDir %>/node_modules/bootstrap/less'),
+    resolve(process.cwd(), '<%= pkg.gruntConfig.commonsUiDir %>/lib/widgets'),
+    resolve(process.cwd(), '<%= pkg.gruntConfig.commonsUiDir %>/resources/less'),
+    resolve(process.cwd(), '<%= pkg.gruntConfig.commonsUiDir %>/resources/css'),
+    resolve(process.cwd(), '<%= pkg.gruntConfig.commonsUiDir %>/node_modules'),
     resolve(process.cwd(), 'camunda-' + pathConfig.appName +'-ui', 'styles'),
     resolve(process.cwd(), 'camunda-' + pathConfig.appName +'-ui', 'client/scripts'),
     resolve(process.cwd(), 'node_modules/camunda-bpm-webapp/camunda-' + pathConfig.appName +'-ui', 'styles'),
     resolve(process.cwd(), 'node_modules/camunda-bpm-webapp/camunda-' + pathConfig.appName +'-ui', 'client/scripts'),
 
-    resolve(__dirname, '../../..', 'camunda-bpm-webapp/webapp/src/main/resources-plugin'),
-    resolve(__dirname, '../../..', 'camunda-bpm-platform-ee/webapps/camunda-webapp/plugins/src/main/resources-plugin')
+    // resolve(__dirname, '../../..', 'camunda-bpm-webapp/webapp/src/main/resources-plugin'),
+    // resolve(__dirname, '../../..', 'camunda-bpm-platform-ee/webapps/camunda-webapp/plugins/src/main/resources-plugin')
   ];
 
   lessConfig[pathConfig.appName + '_styles'] = {
