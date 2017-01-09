a9d0aa4b450107cf65aa2466b67df85b854ff9e5
Christian Lipphardt <christian.lipphardt@googlemail.com>
2017-01-09 13:40:09 +0100

    fix(test-e2e): add 'protractorTests' to Grunt's 'default' target
    related to #CAM-7199

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,7 +132,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('auto-build', ['build', 'connect:widgetTests', 'watch']);
 
-  grunt.registerTask('default', ['build', 'karma', 'connect:widgetTests', 'protractor:widgets']);
+  grunt.registerTask('default', ['build', 'karma', 'protractorTests']);
 
   grunt.registerTask('protractorTests', ['ensureSelenium', 'connect:widgetTests', 'protractor:widgets']);
 
