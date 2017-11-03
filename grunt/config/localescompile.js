6f3f1519595b5b28eaae727a5905799a6e857148
Tassilo Weidner <tassilo.weidner@camunda.com>
2017-11-03 14:04:50 +0100

    chore(*): add i18n support and remove translate with default fallback
    related to CAM-8054

--- grunt/config/localescompile.js
+++ grunt/config/localescompile.js
@@ -8,9 +8,7 @@ module.exports = function(config, localesConfig, pathConfig) {
         anOption: 'for production'
       },
       src: [
-        pathConfig.sourceDir + '/../../camunda-commons-ui/resources/locales/*.json',
-        pathConfig.sourceDir + '/../../camunda-commons-ui/lib/*/locales/*.json',
-        pathConfig.sourceDir + '/scripts/**/locales/*.json'
+        pathConfig.sourceDir + '/../../camunda-commons-ui/lib/locales/*.json'
       ]
   };
 };
