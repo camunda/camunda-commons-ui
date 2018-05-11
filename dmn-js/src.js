dea53cf40730d11c51ddde74e9c96ab7b5bdc7ae
Seif Eddine Ghezala <siffogh3@gmail.com>
2018-05-11 09:25:47 +0200

    fix(cockpit): fix problem with decision table context menu
    Related to CAM-9009

--- dmn-js/src.js
+++ dmn-js/src.js
@@ -1,2 +1,11 @@
+var original = document.addEventListener;
+document.addEventListener = function(...args) {
+  const event = args[0];
+  if(event === 'focusin') {
+    return;
+  }
+  return original.apply(document, args);
+};
+
 export { default as NavigatedViewer } from './NavigatedViewer';
 export { default as Modeler } from 'dmn-js/lib/Modeler';
