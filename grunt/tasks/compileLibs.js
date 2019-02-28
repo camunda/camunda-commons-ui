d16f3ecc30d3f766d8ef3c0065d00f0966b4bbce
Tassilo Weidner <tassilo.weidner@camunda.com>
2019-02-28 16:44:51 +0100

    chore(deps): transpile superagent
    related to CAM-9844

--- grunt/tasks/compileLibs.js
+++ grunt/tasks/compileLibs.js
@@ -1,7 +1,7 @@
 let { promisify } = require('util');
 let exec = promisify(require('child_process').exec);
 let path = require('path');
-let libs = ['bpmn-js', 'dmn-js', 'cmmn-js'];
+let libs = ['node_modules/camunda-bpm-sdk-js/vendor/superagent', 'bpmn-js', 'dmn-js', 'cmmn-js'];
 
 module.exports = function(grunt) {
   grunt.registerTask('compileLibs', function() {
