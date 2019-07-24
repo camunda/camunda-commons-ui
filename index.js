6af2d9373ec10aa2fd879a65a5d8e696786fd5d4
Martin Stamm <martin.stamm@camunda.com>
2019-07-24 11:32:04 +0200

    core(deps): remove unused vendor libs
    related to CAM-10617

--- index.js
+++ index.js
@@ -25,7 +25,6 @@
 'use strict';
 var _ = require('lodash');
 
-
 // ## requirejs
 // @name camunda-commons-ui.requirejs
 //
@@ -44,49 +43,42 @@ function requirejsConf(options) {
   }
 
   var conf = {
-    stubModules: [
-      'text'
-    ],
+    stubModules: ['text'],
 
     paths: {
-      'camunda-commons-ui':         'lib',
+      'camunda-commons-ui': 'lib',
 
       // #### npm dependencies
-      'angular-data-depend':        'node_modules/angular-data-depend/src/dataDepend',
-      'angular-translate':          'node_modules/angular-translate/dist/angular-translate',
-      'angular-moment':             'node_modules/angular-moment/angular-moment',
-      'camunda-bpm-sdk-js':         'node_modules/camunda-bpm-sdk-js/dist/camunda-bpm-sdk-angular',
-      'camunda-bpm-sdk-js-type-utils':   'node_modules/camunda-bpm-sdk-js/dist/camunda-bpm-sdk-type-utils',
-      jquery:                       'node_modules/jquery/dist/jquery',
-      moment:                       'node_modules/moment/moment',
-      requirejs:                    'node_modules/requirejs/require',
-      ngDefine:                     'node_modules/requirejs-angular-define/dist/ngDefine',
-      text:                         'node_modules/requirejs-text/text',
-      lodash:                       'node_modules/lodash/lodash',
-      angular:                      'node_modules/angular/angular',
-      'angular-animate':            'node_modules/angular-animate/angular-animate',
-      'angular-cookies':            'node_modules/angular-cookies/angular-cookies',
-      'angular-loader':             'node_modules/angular-loader/angular-loader',
-      'angular-mocks':              'node_modules/angular-mocks/angular-mocks',
-      'angular-resource':           'node_modules/angular-resource/angular-resource',
-      'angular-route':              'node_modules/angular-route/angular-route',
-      'angular-sanitize':           'node_modules/angular-sanitize/angular-sanitize',
-      'angular-scenario':           'node_modules/angular-scenario/angular-scenario',
-      'angular-touch':              'node_modules/angular-touch/angular-touch',
+      'angular-data-depend': 'node_modules/angular-data-depend/src/dataDepend',
+      'angular-translate':
+        'node_modules/angular-translate/dist/angular-translate',
+      'angular-moment': 'node_modules/angular-moment/angular-moment',
+      'camunda-bpm-sdk-js':
+        'node_modules/camunda-bpm-sdk-js/dist/camunda-bpm-sdk-angular',
+      'camunda-bpm-sdk-js-type-utils':
+        'node_modules/camunda-bpm-sdk-js/dist/camunda-bpm-sdk-type-utils',
+      jquery: 'node_modules/jquery/dist/jquery',
+      moment: 'node_modules/moment/moment',
+      requirejs: 'node_modules/requirejs/require',
+      ngDefine: 'node_modules/requirejs-angular-define/dist/ngDefine',
+      text: 'node_modules/requirejs-text/text',
+      lodash: 'node_modules/lodash/lodash',
+      angular: 'node_modules/angular/angular',
+      'angular-animate': 'node_modules/angular-animate/angular-animate',
+      'angular-cookies': 'node_modules/angular-cookies/angular-cookies',
+      'angular-loader': 'node_modules/angular-loader/angular-loader',
+      'angular-mocks': 'node_modules/angular-mocks/angular-mocks',
+      'angular-resource': 'node_modules/angular-resource/angular-resource',
+      'angular-route': 'node_modules/angular-route/angular-route',
+      'angular-sanitize': 'node_modules/angular-sanitize/angular-sanitize',
+      'angular-scenario': 'node_modules/angular-scenario/angular-scenario',
+      'angular-touch': 'node_modules/angular-touch/angular-touch',
 
       // #### vendor dependencies
-      'angular-bootstrap':          'vendor/ui-bootstrap-tpls-2.5.0-camunda',
-      'jquery-ui-core':             'vendor/jquery.ui.core',
-      'jquery-ui-mouse':            'vendor/jquery.ui.mouse',
-      'jquery-ui-widget':           'vendor/jquery.ui.widget',
-      'jquery-ui-draggable':        'vendor/jquery.ui.draggable',
-      'jquery-mousewheel':          'vendor/jquery.mousewheel',
-      'jquery-overscroll':          'vendor/jquery.overscroll',
-      domReady:                     'vendor/domReady',
-      'placeholders-js':            'vendor/placeholders.main',
-      'prismjs':                    'vendor/prism',
-      'bpmn-io':                    'node_modules/bower-bpmn-js/dist/bpmn-navigated-viewer',
-      'dmn-io':                     'node_modules/dmn-js/dist/dmn-modeler'
+      'angular-bootstrap': 'vendor/ui-bootstrap-tpls-2.5.0-camunda',
+      prismjs: 'vendor/prism',
+      'bpmn-io': 'node_modules/bower-bpmn-js/dist/bpmn-navigated-viewer',
+      'dmn-io': 'node_modules/dmn-js/dist/dmn-modeler'
     },
 
     shim: {
@@ -95,41 +87,27 @@ function requirejsConf(options) {
         exports: 'angular'
       },
 
-      'placeholders-js':      [
-                                'vendor/placeholders.utils.js',
-                                'vendor/placeholders.jquery.js'
-                              ],
-      'camunda-commons-ui':   [
-                                'angular',
-                                'angular-resource',
-                                'angular-route',
-                                'angular-sanitize',
-                                'angular-translate',
-                                'angular-bootstrap',
-                                'moment',
-                                'placeholders-js'
-                              ],
-      'angular-animate':      ['angular'],
-      'angular-cookies':      ['angular'],
-      'angular-loader':       ['angular'],
-      'angular-mocks':        ['angular'],
-      'angular-resource':     ['angular'],
-      'angular-route':        ['angular'],
-      'angular-sanitize':     ['angular'],
-      'angular-scenario':     ['angular'],
-      'angular-touch':        ['angular'],
-      'angular-bootstrap':    ['angular'],
-      'angular-translate':    ['angular'],
-      'jquery-overscroll':    ['jquery'],
-      'jquery-mousewheel':    ['jquery'],
-
-      'jquery-ui-core':       ['jquery'],
-      'jquery-ui-widget':     ['jquery-ui-core'],
-      'jquery-ui-mouse':      ['jquery-ui-widget'],
-      'jquery-ui-draggable':  [
-                                'jquery-ui-widget',
-                                'jquery-ui-mouse'
-                              ]
+      'camunda-commons-ui': [
+        'angular',
+        'angular-resource',
+        'angular-route',
+        'angular-sanitize',
+        'angular-translate',
+        'angular-bootstrap',
+        'moment',
+        'placeholders-js'
+      ],
+      'angular-animate': ['angular'],
+      'angular-cookies': ['angular'],
+      'angular-loader': ['angular'],
+      'angular-mocks': ['angular'],
+      'angular-resource': ['angular'],
+      'angular-route': ['angular'],
+      'angular-sanitize': ['angular'],
+      'angular-scenario': ['angular'],
+      'angular-touch': ['angular'],
+      'angular-bootstrap': ['angular'],
+      'angular-translate': ['angular']
     },
 
     packages: [
@@ -147,36 +125,37 @@ function requirejsConf(options) {
   };
 
   // prefix all the paths
-  _.each(conf.paths, function (val, key) {
+  _.each(conf.paths, function(val, key) {
     conf.paths[key] = options.pathPrefix + '/' + val;
   });
-  _.each(conf.packages, function (val, key) {
+  _.each(conf.packages, function(val, key) {
     if (conf.packages[key].location) {
-      conf.packages[key].location = options.pathPrefix + '/' + conf.packages[key].location;
+      conf.packages[key].location =
+        options.pathPrefix + '/' + conf.packages[key].location;
     }
   });
 
   return conf;
 }
 
-
 function livereloadSnippet(grunt) {
-  return function (data) {
+  return function(data) {
     var buildMode = grunt.config('buildMode');
     var livereloadPort = grunt.config('pkg.gruntConfig.livereloadPort');
     if (buildMode !== 'prod' && livereloadPort) {
-      grunt.log.writeln('Enabling livereload for ' + data.name + ' on port: ' + livereloadPort);
+      grunt.log.writeln(
+        'Enabling livereload for ' + data.name + ' on port: ' + livereloadPort
+      );
       var contents = grunt.file.read(data.path);
 
       contents = contents
-                  .replace(/\/\* live-reload/, '/* live-reload */')
-                  .replace(/LIVERELOAD_PORT/g, livereloadPort);
+        .replace(/\/\* live-reload/, '/* live-reload */')
+        .replace(/LIVERELOAD_PORT/g, livereloadPort);
 
       grunt.file.write(data.path, contents);
     }
   };
 }
-
 
 function builder(grunt) {
   return function(mode) {
@@ -184,27 +163,32 @@ function builder(grunt) {
     var pkg = grunt.config.data.pkg;
     var config = pkg.gruntConfig;
 
-    grunt.config.data.buildTarget = (mode === 'prod' ? config.prodTarget : config.devTarget);
-    grunt.log.subhead('Will build the "'+ pkg.name +'" project in "'+ mode +'" mode and place it in "'+ grunt.config('buildTarget') +'"');
+    grunt.config.data.buildTarget =
+      mode === 'prod' ? config.prodTarget : config.devTarget;
+    grunt.log.subhead(
+      'Will build the "' +
+        pkg.name +
+        '" project in "' +
+        mode +
+        '" mode and place it in "' +
+        grunt.config('buildTarget') +
+        '"'
+    );
     if (mode === 'dev') {
-      grunt.log.writeln('Will serve on port "'+
-        config.connectPort +
-        '" and liverreload available on port "'+
-        config.livereloadPort +
-        '"');
+      grunt.log.writeln(
+        'Will serve on port "' +
+          config.connectPort +
+          '" and liverreload available on port "' +
+          config.livereloadPort +
+          '"'
+      );
     }
 
-    var tasks = [
-      'clean',
-      'copy',
-      'less',
-      'requirejs'
-    ];
+    var tasks = ['clean', 'copy', 'less', 'requirejs'];
 
     grunt.task.run(tasks);
   };
 }
-
 
 module.exports = {
   // @name camunda-commons-ui.utils
